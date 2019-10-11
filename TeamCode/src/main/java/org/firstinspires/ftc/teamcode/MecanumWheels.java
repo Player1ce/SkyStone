ab1017a05c9b8f15b5ba3f16c08afb103e03c5d7
Player1ce <38512653+Player1ce@users.noreply.github.com>
2019-10-10 20:08:27 -0500

    Added mecanum wheels class and also used one shot logic for more functions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumWheels.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+public class MecanumWheels {
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
+
+
+    double frontRightPower; //-right
+    double frontLeftPower; //-right
+    double backRightPower; //-right
+    double backLeftPower;
+
+    double xLeft;
+    double yLeft;
+    double xRight;
+
+    public void initialize(DcMotor frontLeft, DcMotor frontRight,DcMotor backLeft, DcMotor backRight ) {
+      //assign the passed in Motors to the class fields for later use
+      this.frontLeft = frontLeft;
+      this.frontRight = frontRight;
+      this.backLeft = backLeft;
+      this.backRight = backRight;
+
+    }
+
+
+    public void setPower (double frontRightPower, double frontLeftPower, double backRightPower, double backLeftPower) {
+        frontRight.setPower(frontRightPower);
+        frontLeft.setPower(frontLeftPower);
+        backRight.setPower(backRightPower);
+        backLeft.setPower(backLeftPower);
+    }
+
+    public void setPowerFromGamepad(boolean reverse, double power,double left_stick_x,
+                                    double right_stick_x,double left_stick_y) {
+        xLeft=left_stick_x;
+        if (!reverse) {
+            xRight = right_stick_x;
+            yLeft = -left_stick_y;
+        } else {
+            xRight = -right_stick_x;
+            yLeft = left_stick_y;
+        }
+
+        frontRightPower = (yLeft - xRight + xLeft) * power; //-right
+        frontLeftPower = (yLeft + xRight - xLeft) * power; //-right
+        backRightPower = (yLeft + xRight + xLeft) * power; //-right
+        backLeftPower = (yLeft - xRight - xLeft) * power;
+
+        setPower(frontRightPower, frontLeftPower, backRightPower, backLeftPower);
+
+    }
+}
