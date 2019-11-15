8b736926b9f72a8cdd30b8b5c17a0036ef8cd5d5
Player1ce <38512653+Player1ce@users.noreply.github.com>
2019-11-14 20:01:56 -0600

    stop robot when we click stop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankAutonomousRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankAutonomousRed.java
@@ -58,88 +58,88 @@ public class TankAutonomousRed extends LinearOpMode {
     }
 
     protected void executeAutonomousLogic() {
-        double ticksToInches = 288 / (Math.PI * 6.125);
-        mecanumWheels.ForwardMoveInches(telemetry, 0.5, 23, ticksToInches);
-        mecanumWheels.crabDrive("right", 0.7, 1050);
+        while(opModeIsActive()) {
+            double ticksToInches = 288 / (Math.PI * 6.125);
+            mecanumWheels.ForwardMoveInches(telemetry, 0.5, 23, ticksToInches);
+            mecanumWheels.crabDrive("right", 0.7, 1050);
 
-        //now drive until we see the red or blue plate
-        int colors[] = frontColorSensor.getAverageColor(500);
+            //now drive until we see the red or blue plate
+            int colors[] = frontColorSensor.getAverageColor(500);
 
-        int redBaseline = colors[0];
-        int blueBaseline = colors[2];
+            int redBaseline = colors[0];
+            int blueBaseline = colors[2];
 
-        int redTarget = redBaseline + 7;
-        int blueTarget = blueBaseline + 4;
+            int redTarget = redBaseline + 7;
+            int blueTarget = blueBaseline + 4;
 
-        telemetry.addData("Baseline Colors", colors[0] + "  " + colors[1] + "   " + colors[2]);
-        telemetry.update();
-        mecanumWheels.setPower(0.3, 0.3, 0.3, 0.3);
-
-        while (colorSensor.red() < redTarget && colorSensor.blue() < blueTarget) {
-            telemetry.addData("Colors", "-> " + colors[0] + "  " + colors[1] + "   " + colors[2]);
+            telemetry.addData("Baseline Colors", colors[0] + "  " + colors[1] + "   " + colors[2]);
             telemetry.update();
-            sleep(10);
-        }
-        mecanumWheels.StopMotors();
-        telemetry.addData("Colors", "* " + colors[0] + "  " + colors[1] + "   " + colors[2]);
-        telemetry.update();
+            mecanumWheels.setPower(0.3, 0.3, 0.3, 0.3);
 
-        mecanumWheels.ForwardMoveInches(telemetry, 0.4, 1, ticksToInches);
-
-        //mecanumWheels.ForwardMoveInches(telemetry, 0.5, 5, ticksToInches);
-
-        moveHook(ServoPosition.DOWN);
-
-        sleep(1000);
-        mecanumWheels.BackwardMoveInches(telemetry, -0.5, 31, ticksToInches);
-        mecanumWheels.ForwardMoveInches(telemetry, 0.5, .5, ticksToInches);
-
-        sleep(1000);
-
-        moveHook(ServoPosition.UP);
-
-        sleep(500);
-
-        mecanumWheels.crabDrive("left", 0.7, 2500);
-        mecanumWheels.ForwardMoveInches(telemetry, 0.5, 13, ticksToInches);
-        mecanumWheels.crabDrive("right", 0.7, 2000);
-
-        sleep(500);
-
-        mecanumWheels.crabDrive("left", 0.7, 1000);
-
-        sleep(500);
-
-        mecanumWheels.setPowerFromGamepad(false, .7, -10, 0, 0);
-
-        sleep(1050);
-
-        mecanumWheels.StopMotors();
-
-        sleep(500);
-
-        mecanumWheels.setPowerFromGamepad(true, .7, 0, 0, 10);
-
-        startTime = System.currentTimeMillis();
-
-        while (colorSensor.red() < redTarget && colorSensor.blue() < blueTarget && System.currentTimeMillis()-startTime <1400) {
-            telemetry.addData("Colors", "-> " + colors[0] + "  " + colors[1] + "   " + colors[2]);
+            while (colorSensor.red() < redTarget && colorSensor.blue() < blueTarget) {
+                telemetry.addData("Colors", "-> " + colors[0] + "  " + colors[1] + "   " + colors[2]);
+                telemetry.update();
+                sleep(10);
+            }
+            mecanumWheels.StopMotors();
+            telemetry.addData("Colors", "* " + colors[0] + "  " + colors[1] + "   " + colors[2]);
             telemetry.update();
-            sleep(10);
-            i += 1;
-            if (i == 18) {
-                intake.rampServo.setPosition(.2);
+
+            mecanumWheels.ForwardMoveInches(telemetry, 0.4, 1, ticksToInches);
+
+            //mecanumWheels.ForwardMoveInches(telemetry, 0.5, 5, ticksToInches);
+
+            moveHook(ServoPosition.DOWN);
+
+            sleep(1000);
+            mecanumWheels.BackwardMoveInches(telemetry, -0.5, 31, ticksToInches);
+            mecanumWheels.ForwardMoveInches(telemetry, 0.5, .5, ticksToInches);
+
+            sleep(1000);
+
+            moveHook(ServoPosition.UP);
+
+            sleep(500);
+
+            mecanumWheels.crabDrive("left", 0.7, 2500);
+            mecanumWheels.ForwardMoveInches(telemetry, 0.5, 13, ticksToInches);
+            mecanumWheels.crabDrive("right", 0.7, 2000);
+
+            sleep(500);
+
+            mecanumWheels.crabDrive("left", 0.7, 1000);
+
+            sleep(500);
+
+            mecanumWheels.setPowerFromGamepad(false, .7, -10, 0, 0);
+
+            sleep(1050);
+
+            mecanumWheels.StopMotors();
+
+            sleep(500);
+
+            mecanumWheels.setPowerFromGamepad(true, .7, 0, 0, 10);
+
+            startTime = System.currentTimeMillis();
+
+            while (colorSensor.red() < redTarget && colorSensor.blue() < blueTarget && System.currentTimeMillis() - startTime < 1400) {
+                telemetry.addData("Colors", "-> " + colors[0] + "  " + colors[1] + "   " + colors[2]);
+                telemetry.update();
+                sleep(10);
+                i += 1;
+                if (i == 18) {
+                    intake.rampServo.setPosition(.2);
+                }
+
+                if (i == 35) {
+                    return;
+                }
             }
 
-            if (i == 35) {
-                return;
-            }
+            mecanumWheels.StopMotors();
         }
-
-        mecanumWheels.StopMotors();
-
-
-
+        
     }
 
 }
