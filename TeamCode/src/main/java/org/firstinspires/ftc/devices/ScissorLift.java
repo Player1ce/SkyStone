8d477b05b143ec3b2337d8c7549cd9f60ce95201
Ice <Motstorm100@gmail.com>
2020-01-30 19:09:20 -0600

    added lift test

--- TeamCode/src/main/java/org/firstinspires/ftc/devices/ScissorLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/devices/ScissorLift.java
@@ -8,16 +8,29 @@ import org.firstinspires.ftc.logic.ChassisName;
 public class ScissorLift {
     ChassisName chassis;
     public DcMotor liftMotor;
+    public MecanumWheels wheels;
 
-    public void ScissorLift(ChassisName name) {
+    public ScissorLift (ChassisName name, MecanumWheels Wheels) {
+        this.wheels = Wheels;
         chassis = name;
     }
 
     public void initialize(OpMode opMode) {
         liftMotor = opMode.hardwareMap.dcMotor.get("liftMotor");
-        //liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
+
+    public double getPosition () {
+        return liftMotor.getCurrentPosition();
+    }
+
+    public void resetEncoder () {
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        wheels.sleepAndCheckActive(10);
+        liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
 
 
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LiftTest.java
@@ -3,14 +3,17 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.devices.MecanumWheels;
 import org.firstinspires.ftc.devices.ScissorLift;
 import org.firstinspires.ftc.logic.ButtonOneShot;
+import org.firstinspires.ftc.logic.ChassisName;
 
 @TeleOp(name="lift Test", group="Skystone")
 //@Disabled
 public class LiftTest extends OpMode {
-    private final ScissorLift lift = new ScissorLift();
-    private final ButtonOneShot liftControl = new ButtonOneShot();
+    private final MecanumWheels wheels = new MecanumWheels(ChassisName.TANK);
+    private final ScissorLift lift = new ScissorLift(ChassisName.TANK, wheels);
+    private final ButtonOneShot xButton = new ButtonOneShot();
     //Use this class to test new methods and anything else for auto
 
     public void init() {
@@ -20,15 +23,22 @@ public class LiftTest extends OpMode {
     }
 
     public void loop() {
-        if (gamepad2.left_trigger > 0) {
+        if (gamepad1.left_trigger > 0) {
             lift.liftMotor.setPower(1);
         }
-        else if (gamepad2.right_trigger > 0) {
+        else if (gamepad1.right_trigger > 0) {
             lift.liftMotor.setPower(-1);
         }
         else {
             lift.liftMotor.setPower(0);
         }
+
+        if (xButton.isPressed(gamepad1.x)) {
+            lift.resetEncoder();
+        }
+
+        telemetry.addData("position:", lift.getPosition());
+        telemetry.update();
 
     }
 }
