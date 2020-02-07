fd21511735640e0cc8cab5a83c1d5c7f2d698083
Ice <Motstorm100@gmail.com>
2020-02-06 20:23:19 -0600

    added class for lift distance sensor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/devices/DistanceSensor.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.devices;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.logic.BasicPositions;
+import org.firstinspires.ftc.logic.ChassisName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class DistanceSensor {
+    com.qualcomm.robotcore.hardware.DistanceSensor distanceSensor;
+
+    public DistanceSensor(ChassisName name, MecanumWheels Wheels) {
+
+    }
+
+    public void initialize(OpMode opMode) {
+        com.qualcomm.robotcore.hardware.DistanceSensor distanceSensor = opMode.hardwareMap.get(com.qualcomm.robotcore.hardware.DistanceSensor.class, "liftDistanceSensor");
+        //this.distanceSensor = (Rev2mDistanceSensor)distanceSensor;
+    }
+
+    public void getDistance () {
+        double distance = distanceSensor.getDistance(DistanceUnit.INCH);
+    }
+
+}
