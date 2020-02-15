61af8b8252cd7d2369103d7ca4e37a907d302898
Player1ce <38512653+Player1ce@users.noreply.github.com>
2020-02-15 17:04:57 -0600

    added skystone lever device + tester

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/devices/SkystoneLever.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.devices;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.logic.BasicPositions;
+import org.firstinspires.ftc.logic.ChassisName;
+
+public class SkystoneLever {
+    ChassisName chassis;
+    public Servo leverServo;
+
+    public void initialize(OpMode opMode) {
+        leverServo = opMode.hardwareMap.servo.get("skystoneServo");
+    }
+    public void setPosition (BasicPositions position) {
+        if (position == BasicPositions.OPEN) {
+            leverServo.setPosition(0.65);
+        }
+        if (position == BasicPositions.CLOSED) {
+            leverServo.setPosition(.4);
+        }
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SkystoneLeverTester.java
@@ -0,0 +1,69 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.devices.BlockClaw;
+import org.firstinspires.ftc.devices.SkystoneLever;
+import org.firstinspires.ftc.logic.BasicPositions;
+import org.firstinspires.ftc.logic.ButtonOneShot;
+import org.firstinspires.ftc.logic.ChassisName;
+
+import static org.firstinspires.ftc.logic.BasicPositions.*;
+
+@TeleOp(name = "Skystone Lever Tester", group = "Skystone")
+public class SkystoneLeverTester extends OpMode {
+    private final SkystoneLever lever = new SkystoneLever();
+    private final ButtonOneShot buttonOneShotY = new ButtonOneShot();
+    private final ButtonOneShot buttonOneShotX = new ButtonOneShot();
+    private double leverPosition;
+    private boolean leverUp;
+    private String leverState;
+    private boolean setPositions;
+
+    public void init() {
+        lever.initialize(this);
+
+    }
+
+    public void loop() {
+        if (gamepad1.left_bumper) {
+            if (leverPosition < 1) {
+                leverPosition = leverPosition + 0.001;
+            }
+            lever.leverServo.setPosition(leverPosition);
+        }
+        if (gamepad1.right_bumper) {
+            if (leverPosition > .0001) {
+                leverPosition = leverPosition - 0.001;
+            }
+            lever.leverServo.setPosition(leverPosition);
+        }
+
+        if (buttonOneShotY.isPressed(gamepad1.y)) {
+            leverUp = !leverUp;
+        }
+
+        if (buttonOneShotX.isPressed(gamepad1.x)) {
+            setPositions = !setPositions;
+        }
+        if (setPositions) {
+            if (leverUp) {
+                lever.setPosition(OPEN);
+                leverState = "open";
+            } else if (!leverUp) {
+                lever.setPosition(CLOSED);
+                leverState = "closed";
+            }
+        }
+
+        telemetry.addData("claw Position:", lever.leverServo.getPosition());
+        telemetry.addData("leverState:", leverState);
+        telemetry.addData("setPositions:", setPositions);
+        telemetry.update();
+        
+        
+        
+    }
+
+}
