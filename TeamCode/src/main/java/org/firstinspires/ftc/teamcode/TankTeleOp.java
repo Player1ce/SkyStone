3c1a3fc828143a9deb1be92d77ceddcfea2c2b2c
Player1ce <38512653+Player1ce@users.noreply.github.com>
2020-02-18 16:57:15 -0600

    began remapping in tank teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankTeleOp.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.devices.Encoders;
 import org.firstinspires.ftc.devices.IMURevHub;
+import org.firstinspires.ftc.devices.SkystoneLever;
+import org.firstinspires.ftc.logic.BasicPositions;
 import org.firstinspires.ftc.logic.ButtonOneShot;
 import org.firstinspires.ftc.logic.ChassisName;
 import org.firstinspires.ftc.devices.FoundationHook;
@@ -20,23 +22,22 @@ public class TankTeleOp extends OpMode {
     private final SkystoneIntake intake = new SkystoneIntake(ChassisName.TANK);
     private final FoundationHook hookServo = new FoundationHook(ChassisName.TANK);
     private final IMURevHub imu = new IMURevHub(ChassisName.TANK);
+    private final SkystoneLever skystoneLever = new SkystoneLever();
 
     private ButtonOneShot reverseButtonLogic = new ButtonOneShot();
     private ButtonOneShot powerChangeButtonLogic = new ButtonOneShot();
     private ButtonOneShot hookServoButtonLogic = new ButtonOneShot();
-    private ButtonOneShot rampServoButtonLogic = new ButtonOneShot();
-    private ButtonOneShot rampDirectControl = new ButtonOneShot();
+    private ButtonOneShot skystoneLeverButtonLogic = new ButtonOneShot();
+    private ButtonOneShot hookControlButtonLocgic = new ButtonOneShot();
     private Encoders encoders = new Encoders(ChassisName.TANK);
 
     //TODO correct starting cars for drive
     private boolean reverse = true;
     private boolean highPower = true;
     private boolean hookServoEnable = false;
-    private boolean rampServoUp = false;
-    private boolean directRampControl = false;
     private final double HIGH_POWER = 1.0;
     private final double NORMAL_POWER = 0.5;
-    private double rampPosition;
+    private boolean leverUP = true;
 
 
     public void init() {
@@ -56,6 +57,17 @@ public class TankTeleOp extends OpMode {
     }
 
     public void loop() {
+        /** Drive Controller (gamepad1) ---------------------------
+         * Controller for the driver:
+         * a.	B: Reverse
+         * b.	A: Power Change
+         * c.	Y: Skystone Lever
+         * d.	X: Hook
+         * e.	Lt: Intake In
+         * f.	Lr: Intake Out
+         * g.	Lb:
+         * h.	Rb:
+         */
 
         //drive train --------------------------------
         if (powerChangeButtonLogic.isPressed(gamepad1.a)) {
@@ -66,6 +78,8 @@ public class TankTeleOp extends OpMode {
         }
         //if high power, use the high power constant, else use the normal power constant
         double power = highPower ? HIGH_POWER : NORMAL_POWER;
+
+
 
         mecanumWheels.setPowerFromGamepad(reverse, power,gamepad1.left_stick_x,
                 gamepad1.right_stick_x,gamepad1.left_stick_y);
@@ -83,38 +97,16 @@ public class TankTeleOp extends OpMode {
             //hookServo.setPosition(.47);
         }
 
-        //gamepad 2 functions-------------------
-
-        //ramp servo control -------------------
-        if (rampServoButtonLogic.isPressed(gamepad2.y)) {
-            rampServoUp = !rampServoUp;
+        //Skystone Lever -------------------------------
+        if (skystoneLeverButtonLogic.isPressed(gamepad1.y)) {
+            leverUP = !leverUP;
         }
-        if (rampDirectControl.isPressed(gamepad2.b)) {
-            directRampControl = !directRampControl;
+        if (leverUP) {
+            skystoneLever.setPosition(BasicPositions.UP);
         }
-        if (!directRampControl) {
-            if (rampServoUp) {
-                intake.rampServo.setPosition(.38);
-            } else {
-                intake.rampServo.setPosition(.29);
-            }
+        else if (!leverUP) {
+            skystoneLever.setPosition(BasicPositions.DOWN);
         }
-        if (directRampControl) {
-            if (gamepad2.left_bumper) {
-                if (rampPosition < .5) {
-                    rampPosition = rampPosition + 0.003;
-                }
-                intake.rampServo.setPosition(rampPosition);
-            }
-            if (gamepad2.right_bumper) {
-                if (rampPosition > .001) {
-                    rampPosition = rampPosition - 0.003;
-                }
-                intake.rampServo.setPosition(rampPosition);
-            }
-        }
-
-
 
         //intake control ----------------------------
         if (gamepad2.left_trigger > 0) {
@@ -130,6 +122,22 @@ public class TankTeleOp extends OpMode {
             intake.rightIntake.setPower(0);
         }
 
+        /** I/O Controller Controller (gmaepad2)-------------------
+         * Controller for intake and output:
+         * a.	A: Swivel 180
+         * b.	B: Swivel 90
+         * c.	Y: Recalibrate SL
+         * d.	X: Scissor Lift Set Positions
+         * e.	Lt: Down SL
+         * f.	Rt: Up SL
+         * g.	Lb: Open Claw
+         * h.	Rb: Close Claw
+         */
+
+        
+
+
+
 
         //telemetry ------------------------------
         //telemetry is used to show on the driver controller phone what the code sees
@@ -138,13 +146,14 @@ public class TankTeleOp extends OpMode {
 
         telemetry.addData("Power:", power);
         telemetry.addData("F/R:", robot.reverseSense(reverse));
-        telemetry.addData("rampServoPosition:", rampPosition);
         telemetry.addData("hookServo Position", hookServo.hookServo.getPosition());
-        telemetry.addData("rampServo Position:", intake.rampServo.getPosition());
         telemetry.addData("x_left:", mecanumWheels.xLeft);
         telemetry.addData("x_right:", mecanumWheels.xRight);
         telemetry.addData("y_left:", mecanumWheels.yLeft);
         telemetry.addData("y:", encoders.getY());
+
+        //telemetry.addData("rampServo Position:", intake.rampServo.getPosition());
+        //telemetry.addData("rampServoPosition:", rampPosition);
 
         /* not important in current system
         if (rampServoUp) {
