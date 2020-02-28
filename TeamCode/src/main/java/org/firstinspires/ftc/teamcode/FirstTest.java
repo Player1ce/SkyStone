f1b8698b9d48354341f8c13889040bdcb45089e7
Ice <Motstorm100@gmail.com>
2020-02-27 22:37:46 -0600

    Testing for orignial pid

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTest.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.devices.MecanumWheels;
+import org.firstinspires.ftc.logic.ChassisName;
+import org.firstinspires.ftc.logic.Navigation;
+
+@Autonomous(name = "FirstTest", group = "Skystone")
+public class FirstTest extends LinearOpMode {
+    private final MecanumWheels wheels = new MecanumWheels(ChassisName.TANK);
+    private final Navigation navigation = new Navigation(ChassisName.TANK);
+
+    public void init() {
+        wheels.initializeWheels(this);
+    }
+
+    public void runOpMode() {
+        navigation.NavigateStraightTicks(telemetry,.7, .3,500);
+        wheels.sleepAndCheckActive(2000);
+        navigation.NavigateStraightTicks(telemetry,.7,.3,-500);
+        wheels.sleepAndCheckActive(2000);
+
+    }
+
+
+}
